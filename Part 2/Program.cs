4cfb49ec1521f60c2f8ded697648a70033953a48
polakovap <S00239846@atu.ie>
2023-02-01 13:47:37 +0000

    Worksheet done

--- Part 2/Program.cs
+++ Part 2/Program.cs
@@ -8,7 +8,41 @@ namespace Part_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.Write("The hours used (-999 to exit): ");
+            int hours = int.Parse(Console.ReadLine());
+            double total = 0;
+
+            while (hours != -999)
+            {
+                Console.WriteLine($"Charge: {CalculateCharge(hours):c}");
+                
+                total += CalculateCharge(hours); //total += hours is the same as total = total + hours
+
+                Console.WriteLine($"Total charges: {total:c}");
+
+                Console.Write("The hours used (-999 to exit): ");
+                hours = int.Parse(Console.ReadLine());
+            }
+        }
+        static double CalculateCharge (int numOfHours) //method to calculate the charges for the user
+        {
+            double charge = 0;
+            if (numOfHours<=3)
+            {
+                charge = 2;
+            }
+            else
+            {
+                charge = 2 + (numOfHours - 3) * 0.5;
+            }
+            if (charge >= 10)
+            {
+                charge = 10;
+            }
+            return charge;
         }
     }
 }
